2c08ef1b937825a38941499fd4502dc9b790ca12
Charan <61114770+akulacharan@users.noreply.github.com>
2026-09-16 19:49:19 +0530

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -91,7 +91,10 @@ def browser_context(request):
 
     # Launch the specified browser
     if browser_name.lower() == "chromium":
-        browser = playwright.chromium.launch(headless=not headed_flag)
+        browser = playwright.chromium.launch(
+    channel="chrome",
+    headless=not headed_flag
+)
     elif browser_name.lower() == "firefox":
         browser = playwright.firefox.launch(headless=not headed_flag)
     elif browser_name.lower() == "webkit":
